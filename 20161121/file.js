123d3f3b834e61ce253688c760c5e1ea706723d3
qinchun007 <778741954@qq.com>
2016-11-28 13:00:41 +0800

    2016/11/27

--- 20161121/file.js
+++ 20161121/file.js
@@ -1,21 +1,22 @@
 var fs = require('fs');
+var a;
 console.log(__dirname);
-var filename = __dirname +'\\web\\index.html';
+var filename = __dirname + '\\web\\index.html';
 console.log(filename);
-function readfile()
-{fs.readFile(filename,'utf8',function(err,content){
-    if(err){
+
+fs.readFile(filename, 'utf8', function(err, content) {
+    if (err) {
         console.log('Failed to read');
         return;
     }
     console.log(content);
-    return content;
-});}
+    a = content;
+});
 var http = require('http');
-var server = http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Tpye':'text/plain'});
-  var file = readfile();
-    res.write(file+'');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Tpye': 'text/html' });
+
+    res.write(a);
     res.end();
 });
 server.listen(12345);
