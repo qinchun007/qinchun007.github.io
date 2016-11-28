15a420e1f038675b229f92968cdf80981a27d2a8
qinchun007 <778741954@qq.com>
2016-11-28 15:47:59 +0800

    2016/11/28_1

--- 20161121/file.js
+++ 20161121/file.js
@@ -4,20 +4,28 @@ console.log(__dirname);
 var filename = __dirname + '\\web\\index.html';
 console.log(filename);
 
-fs.readFile(filename, 'utf8', function(err, content) {
-    if (err) {
-        console.log('Failed to read');
-        return;
-    }
-    console.log(content);
-    a = content;
-});
+
 var http = require('http');
 var server = http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Tpye': 'text/html' });
 
-    res.write(a);
-    res.end();
+    fs.readFile(filename, 'utf8', function(err, content) {
+        if (err) {
+            console.log('Failed to read');
+            res.writeHead(404, { 'Content-Tpye': 'text/html' });
+            res.end();
+            return;
+        }
+        //console.log(content);
+        // a = content;
+        res.write(content);
+        res.end();
+    });
+
+
+
+
 });
+
+
 server.listen(12345);
 console.log('server running at http://127.0.0.1:12345/')
